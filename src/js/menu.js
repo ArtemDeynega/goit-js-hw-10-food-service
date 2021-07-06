778391031b29fcaa3428d04b2802f5839852b4ac
Artem Deynega <artemdeynega892@gmail.com>
2021-07-06 23:09:28 +0300

    сделал меню

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,4 +1,6 @@
 import upcomingMenu from '../menu.json';
 import menuTemplate from '../templates/menu.hbs';
 
-console.log(menuTemplate(upcomingMenu));
+const jsMenu = document.querySelector('.js-menu');
+
+jsMenu.insertAdjacentHTML('beforeend', menuTemplate(upcomingMenu));
